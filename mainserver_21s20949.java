2e755f0de7eed26bf1b242a7f8f322529d99e37a
AzzaAlowaisi <135142568+AzzaAlowaisi@users.noreply.github.com>
2023-05-31 18:50:00 +0400

    Create mainserver_21s20949.java

---
+++ mainserver_21s20949.java
@@ -0,0 +1,9 @@
+public class mainserver_21s20949 {
+     public static void main(String args[])
+    {
+        server_21s20949 s=new server_21s20949();
+        Thread a=new Thread(s);
+        a.start();
+    }
+    
+}
