c4a36af23d84f1a5335a318b2e806323fd7443e9
AzzaAlowaisi <135142568+AzzaAlowaisi@users.noreply.github.com>
2023-05-31 18:48:44 +0400

    Create mainClient_21s20949.java

---
+++ mainClient_21s20949.java
@@ -0,0 +1,16 @@
+public class mainClient_21s20949
+{
+    public static void main(String args[])
+           
+        {
+       Client_21s20949 s=new Client_21s20949();
+       Thread a=new Thread(s);
+       a.start();
+            
+        }
+   
+
+
+}
+
+
